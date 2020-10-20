a97b1e487af889791bbabe7ffedbedd5bafdbc22
michmitz <stermitzm@gmail.com>
2020-10-20 15:16:30 -0700

    Added mapping function and state to ListPage

--- src/components/ListPage/ListPage.jsx
+++ src/components/ListPage/ListPage.jsx
@@ -1,16 +1,31 @@
 import React, { Component } from 'react'
 
 export default class ListPage extends Component {
+  state = {
+    characters: []
+  }
+  
   handleClick = () => {
     fetch('https://xfiles-api.herokuapp.com/api/v1/characters?category=Main_characters')
     .then(res => res.json())
-    .then(json => console.log(json));
+    .then(json =>
+      this.setState({ 
+        characters: json.results 
+      }));
   }
   
   render() {
+    const { characters } = this.state;
     return (
       <div>
         <button onClick={this.handleClick}>Show Me XFiles Characters</button>
+        <div id="list-display">
+          {
+            characters.map(character => {
+              return <li>{character.name}</li>
+            })
+          }
+        </div>
       </div>
     )
   }
