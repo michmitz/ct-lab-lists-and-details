af8fcc15e0fb84a88700a60c8bf806488f147f01
michmitz <stermitzm@gmail.com>
2020-10-20 14:39:39 -0700

    Added fetch to API

--- src/components/ListPage/ListPage.jsx
+++ src/components/ListPage/ListPage.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
 
 export default class ListPage extends Component {
+  handleClick = () => {
+    fetch('https://xfiles-api.herokuapp.com/api/v1/characters?category=Main_characters')
+    .then(res => res.json())
+    .then(json => console.log(json));
+  }
+  
   render() {
     return (
       <div>
-        <button>Show Me XFiles Characters</button>
+        <button onClick={this.handleClick}>Show Me XFiles Characters</button>
       </div>
     )
   }
