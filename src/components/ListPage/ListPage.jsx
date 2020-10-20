f6acc7f3e50e7fcb13c83828327c92ee2221c9f2
michmitz <stermitzm@gmail.com>
2020-10-20 14:26:07 -0700

    Added ListPage with button

---
+++ src/components/ListPage/ListPage.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ListPage extends Component {
+  render() {
+    return (
+      <div>
+        <button>Show Me XFiles Characters</button>
+      </div>
+    )
+  }
+}
--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import ListPage from '../ListPage/ListPage';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return <ListPage />;
 }
